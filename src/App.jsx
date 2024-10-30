fe07f350657b6423070e304f013603625d5de6bc
vishalmet <vishalmet003@gmail.com>
2024-10-30 14:18:23 +0530

    fh

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import WalletConnect from './components/Signer';
+
+function App() {
+  return (
+    <div className="App">
+      <WalletConnect />
+    </div>
+  );
+}
+
+export default App;
